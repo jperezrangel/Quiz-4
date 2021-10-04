a47eb1e7c6568ab28adf76b047f39532fd080ea0
jperezrangel <88467921+jperezrangel@users.noreply.github.com>
2021-10-04 14:15:01 -0500

    Ajustes al Quiz4.

--- assignments/25TrianguloAsteriscos/src/exercise.py
+++ assignments/25TrianguloAsteriscos/src/exercise.py
@@ -1,24 +1,10 @@
 def print_triangle(height):
-    # Each level of the triangle
-    for level in range(1, height + 1):
-        line = ''
-        # Print the spaces to the left
-        for _ in range(height - level):
-            line += ' '
-        # Print the stars
-        for _ in range(1, level):
-            line += '*'
-        # Add the last * without a space
-        line += '*'
-        # Move to the next level
-        print(line)
-
+    # Teclear el código abajo de la línea.
+    pass
 
 def main():
-    height = int(input("Enter triangle height: "))
-    #escribe tu código abajo de esta línea
-    print_triangle(height)
-
+    # Teclear el código abajo de la línea.
+    pass
 
 if __name__=='__main__':
     main()
